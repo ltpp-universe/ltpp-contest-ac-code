178a741d3fea687021527c0770b8a602803d5266
尤雨东 <root@ltpp.vip>
2024-06-07 20:31:58 +0800

    feat:update

--- sh/init.sh
+++ sh/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git remote add ltpp ssh://git@git.ltpp.vip:66/hbnuacm/ltpp-contest-ac-code.git;
-git remote add github git@github.com:wmzn-ltpp/ltpp-contest-ac-code.git;
+git remote add github git@github.com:ltpp-universe/ltpp-contest-ac-code.git;
