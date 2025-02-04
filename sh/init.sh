824ad51b233f017a50f3c64961589b7c954ff114
尤雨东 <root@ltpp.vip>
2025-02-04 09:09:38 +0800

    feat: sh

--- sh/init.sh
+++ sh/init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-git remote add ltpp ssh://git@git.ltpp.vip:66/hbnuacm/ltpp-contest-ac-code.git;
+git remote add jihulab ssh://git@git.ltpp.vip:66/hbnuacm/ltpp-contest-ac-code.git;
 git remote add github git@github.com:ltpp-universe/ltpp-contest-ac-code.git;
+git remote add origin git@github.com:ltpp-universe/ltpp-contest-ac-code.git;
