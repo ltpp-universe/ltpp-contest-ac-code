ba1cba8cd7ca2807c402c8c10110690ca996425f
尤雨东 <root@ltpp.vip>
2025-02-07 23:08:37 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
+git config core.ignorecase false;
 git remote add gitee git@gitee.com:ltpp-universe/ltpp-contest-ac-code.git;
-git remote add github git@github.com:ltpp-universe/ltpp-contest-ac-code.git;
 git remote add origin git@github.com:ltpp-universe/ltpp-contest-ac-code.git;
