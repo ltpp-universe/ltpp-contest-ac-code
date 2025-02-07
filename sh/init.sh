b99d513c16d8fe9f7a3e3cbc7962e1892949c143
尤雨东 <root@ltpp.vip>
2025-02-07 23:05:55 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git remote add gitee git@git@git.ltpp.vip:66/hbnuacm/ltpp-contest-ac-code.git;
+git remote add gitee git@gitee.com:ltpp-universe/ltpp-contest-ac-code.git;
 git remote add github git@github.com:ltpp-universe/ltpp-contest-ac-code.git;
 git remote add origin git@github.com:ltpp-universe/ltpp-contest-ac-code.git;
